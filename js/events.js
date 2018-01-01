e809a484898f74a16acb7cb95c60344096b2444f
Stephane <son.trinh.dev@gmail.com>
2018-01-01 04:28:01 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,10 +1,17 @@
 //define functions here
 function getIt() {
+<<<<<<< HEAD
   $('p').on('click', alert('Hey!'))
+=======
+  $('p').on('click', function(e) {
+    alert('Hey!');
+  }
+>>>>>>> 22c83fac4195fcc5bfd515c8562069bbe63f9b3c
 }
 
 function frameIt() {
   $('img').on('load', function () {
+<<<<<<< HEAD
     $('img').addClass('tasty')
   })
 }
@@ -20,13 +27,21 @@ function pressIt() {
 function submitIt() {
   $('input').on('submit', alert('Your form is going to be submitted now.'))
 }
+=======
+
+  })
+}
+>>>>>>> 22c83fac4195fcc5bfd515c8562069bbe63f9b3c
 
 $(document).ready(function(){
 
 // call functions here
   getIt();
+<<<<<<< HEAD
   frameIt();
   pressIt();
   submitIt();
+=======
+>>>>>>> 22c83fac4195fcc5bfd515c8562069bbe63f9b3c
 
 });
